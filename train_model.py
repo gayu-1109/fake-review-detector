665769c5885aafc3c4ad29b5a45846312d3a93a6
Gayatri Jadhav <jadhavgayatri1109@gmail.com>
2025-08-16 23:30:36 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -8,7 +8,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, accuracy_score
 
-
 df = pd.read_csv("fake_reviews_dataset.csv")
 
 text_column = 'text_'
@@ -26,12 +25,10 @@ def clean_text(text):
 
 df['cleaned'] = df[text_column].astype(str).apply(clean_text)
 
-# 3. Split the dataset
 X = df['cleaned']
 y = df[label_column]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
 
 vectorizer = TfidfVectorizer(max_features=5000)
 X_train_vec = vectorizer.fit_transform(X_train)
